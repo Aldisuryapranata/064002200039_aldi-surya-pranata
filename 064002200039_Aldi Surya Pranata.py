32b990b731798d3ac5faf54c1b5a4c002e628992
Aldisuryapranata <114167423+Aldisuryapranata@users.noreply.github.com>
2022-09-22 21:49:36 +0700

    Add files via upload

---
+++ 064002200039_Aldi Surya Pranata.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*
+
+#Alamat
+print("\n Alamat Kos Aldi")
+aldi = ("Alamat : Susilo 2a no.10 Grogol")
+
+print(print)
+
+#Persegi
+print("\nMENGHITUNG LUAS PERSEGI ")
+
+panjang = int(input("\nMasukan Panjang: "))
+lebar = float(input("Masukan Lebar: "))
+
+luas = panjang*lebar
+
+print("\nLuas Persegi Panjang :",luas)
